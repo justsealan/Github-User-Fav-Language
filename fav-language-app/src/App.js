df0b48248521c5d8a4fa642816dab96728140730
justsealan <justsealan@gmail.com>
2022-05-31 04:01:23 +0100

    Init react

---
+++ fav-language-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Favourite Language App</h1>
+      </header>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
